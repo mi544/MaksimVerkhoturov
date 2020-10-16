3fb3bba69d42cdeccb9507a9434928d95b841238
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-16 04:34:02 -0600

    initial structure - next js with styled components

---
+++ pages/projects/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Link from "next/link";
+
+import Header from "../../components/Header";
+
+const ProjectsPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Projects</title>
+      </Head>
+
+      <Header />
+
+      <h1>Projects</h1>
+    </>
+  );
+};
+
+export default ProjectsPage;

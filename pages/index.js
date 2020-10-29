0d029f27d4911e3c4d84754960d8a583a3f6b37d
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-28 23:17:31 -0600

    fresh npx create-next-app --example with-styled-components with-styled-components-app

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,10 @@
-import Head from "next/head";
-import styled from "styled-components";
-
-import Header from "../components/Header";
+import styled from 'styled-components'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`;
+`
 
-const Home = () => {
-  return (
-    <>
-      <Head>
-        <title>About Me</title>
-      </Head>
-
-      <Header />
-
-      <Title>Home</Title>
-    </>
-  );
-};
-
-export default Home;
+export default function Home() {
+  return <Title>My page</Title>
+}

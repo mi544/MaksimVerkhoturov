2d522014e44f10eab443d6df7646a0689e8e5516
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-16 05:05:11 -0700

    changing colors, style for nav bar buttons

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 
 const HeaderWrapper = styled.header`
@@ -8,8 +9,8 @@ const HeaderWrapper = styled.header`
   justify-content: space-between;
   padding: 0 16px;
   box-sizing: border-box;
-  background: #8affff;
-  border-bottom: 2px solid cyan;
+  background: black;
+  border-bottom: 2px solid ${p => p.theme.primaryColor};
 `;
 
 const NavBar = styled.nav`
@@ -25,16 +26,21 @@ const NavBarWrapper = styled.ul`
 
 const NavBarItem = styled.li`
   margin: auto 5px;
-  background-color: #808080;
+  background-color: ${p => (p.active ? p.theme.secondaryColor : p.theme.secondaryColor)};
+  box-shadow: ${p => (p.active ? "inset -4px -6px #000000" : "none")};
   border-radius: 6px;
   padding: 10px 15px;
-  border: 1px dashed white;
+  border: 1px solid white;
   clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 23%, 85% 0);
+  transition: 0.2s;
+  &:hover {
+    transform: scale(1.05, 1.05);
+  }
 `;
 
 const NavLink = styled.a`
-  color: #e8e8e8;
-  text-shadow: 2px 1.5px 0px black, 2px 1px 0px black;
+  color: black;
+  /* text-shadow: 2px 1.5px 0px black, 2px 1px 0px black; */
   font-size: 1.5em;
   font-weight: 500;
   &:link {
@@ -58,27 +64,30 @@ const HeaderTitle = styled.span`
 `;
 
 const Header = () => {
+  const router = useRouter();
+  console.log(router.pathname);
+  console.log(router.pathname === "/");
   return (
     <HeaderWrapper>
       <HeaderTitle>Maksim Verkhoturov</HeaderTitle>
       <NavBar>
         <NavBarWrapper>
-          <NavBarItem>
+          <NavBarItem active={router.pathname === "/"}>
             <Link href="/" passHref>
               <NavLink>Home</NavLink>
             </Link>
           </NavBarItem>
-          <NavBarItem>
-            <Link href="/about" passHref>
+          <NavBarItem passHref active={router.pathname === "/about"}>
+            <Link href="/about">
               <NavLink>About</NavLink>
             </Link>
           </NavBarItem>
-          <NavBarItem>
+          <NavBarItem active={router.pathname === "/projects"}>
             <Link href="/projects" passHref>
               <NavLink>Projects</NavLink>
             </Link>
           </NavBarItem>
-          <NavBarItem>
+          <NavBarItem active={router.pathname === "/resources"}>
             <Link href="/resources" passHref>
               <NavLink>Resources</NavLink>
             </Link>

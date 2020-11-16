efc2f58b16d1315aebed25f7d3bc2cff3ff98bf6
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-16 05:01:40 -0700

    restructuring cards, changing text

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-import { Header, Footer, Main, Card, H1, H4, Row, Container } from "components";
+import { Header, Footer, Main, Card, H1, H2, H4, Row, Container } from "components";
 
 const Home = () => {
   return (
@@ -12,25 +12,21 @@ const Home = () => {
         <Header />
         <Main>
           <Row margin="7vh auto auto 6%">
-            <Card width="40%" height="200px">
+            <Card width="40%" height="200px" appear={1.4}>
               <H1>Software developer</H1>
               <H4>Hello! I develop apps.</H4>
             </Card>
           </Row>
           <Row margin="10vh 10% auto 6%">
-            <Card width="20%">
-              <H1>Skills</H1>
-              <H4>Different icons can be here</H4>
-            </Card>
-            <Card height="40vh" width="60%" margin="auto auto auto 10%">
+            <Card height="40vh" width="60%" margin="auto 0 auto auto" appear={2}>
               <H1>Projects</H1>
               <H4>This is where all my projects will be</H4>
             </Card>
           </Row>
           <Row margin="15vh auto auto 6%">
-            <Card width="60%" height="60vh">
+            <Card width="60%" height="60vh" appear={2.7}>
               <H1>Contact me</H1>
-              <H4>Information, resume. And links.</H4>
+              <H2>sd32@pm.me</H2>
             </Card>
           </Row>
         </Main>

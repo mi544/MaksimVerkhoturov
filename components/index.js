787663cd7c872521036dffa97b0c1ed10eac6a9d
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-16 05:04:37 -0700

    adjusting exports

--- components/index.js
+++ components/index.js
@@ -3,6 +3,7 @@ export * from "./Footer";
 export * from "./Main";
 export * from "./Card";
 export * from "./H1";
+export * from "./H2";
 export * from "./H4";
 export * from "./Row";
 export * from "./Container";

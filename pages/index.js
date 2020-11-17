010e15c058db6a8417dd17abea7aed1d96215a93
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-17 02:41:09 -0700

    accounting for changed prop in component

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,19 @@ const Home = () => {
         <Header />
         <Main>
           <Row margin="7vh auto auto 6%">
-            <Card width="40%" height="200px" appear={1.4}>
+            <Card width="40%" height="200px" appearDuration={1.4}>
               <H1>Software developer</H1>
               <H4>Hello! I develop apps.</H4>
             </Card>
           </Row>
           <Row margin="10vh 10% auto 6%">
-            <Card height="40vh" width="60%" margin="auto 0 auto auto" appear={2}>
+            <Card height="40vh" width="60%" margin="auto 0 auto auto" appearDuration={2}>
               <H1>Projects</H1>
               <H4>This is where all my projects will be</H4>
             </Card>
           </Row>
           <Row margin="15vh auto auto 6%">
-            <Card width="60%" height="60vh" appear={2.7}>
+            <Card width="60%" height="60vh" appearDuration={2.7}>
               <H1>Contact me</H1>
               <H2>sd32@pm.me</H2>
             </Card>

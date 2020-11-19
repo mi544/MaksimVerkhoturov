b95eddf3c1b2a50b4ab94f8cdd61029d3176c5a8
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-19 03:00:11 -0700

    adding basic layout

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,17 +1,57 @@
 import Head from "next/head";
+import Image from "next/image";
+import SMLCheck from "utils/hooks/SmallMediumLargeCheck";
 
-import { Header, Main, Container, Footer } from "components";
+import {
+  Header,
+  Main,
+  Container,
+  BorderlessCard,
+  GlitchingTitle,
+  Footer,
+  Row,
+  H1,
+  H2,
+  H4
+} from "components";
 
 const About = () => {
   return (
     <>
       <Head>
-        <title>About me</title>
+        <title>About</title>
       </Head>
       <Container>
         <Header />
         <Main>
-          <h1>About Me</h1>
+          <Row center>
+            <BorderlessCard width="90%">
+              <div>
+                <H2>Maksim Verkhoturov</H2>
+                <article>
+                  <p>
+                    最近、よく耳にする『糖質制限』なる言葉。大豆粉を購入したので、おからパウダーと合わせ、買い置きのココナッツも加えてホットケーキ（パンケーキ）を焼いてみることにしました。
+                  </p>
+                  <p>
+                    牛乳→水にする時はバターと黒蜜だけでも美味しいですがトッピングは栗や生クリームなども♫お好きな和風を楽しんで下さい^o^
+                  </p>
+                  <p>
+                    の時さっくり混ぜるとふわっとした仕上がりになります。そば粉を入れずに薄力粉のみでもOK
+                  </p>
+                </article>
+              </div>
+              <div>
+                <Image
+                  src="/images/tortoise-mobile.png"
+                  alt="tortoise"
+                  width={600}
+                  height={600}
+                  layout="intrinsic"
+                  quality={100}
+                />
+              </div>
+            </BorderlessCard>
+          </Row>
         </Main>
         <Footer />
       </Container>

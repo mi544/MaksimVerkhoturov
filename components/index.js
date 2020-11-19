e739542b57fbd283906a6b1fb49724fb0e5b7545
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-19 02:56:36 -0700

    glitchingTitle component

--- components/index.js
+++ components/index.js
@@ -3,6 +3,7 @@ export * from "./Footer";
 export * from "./Main";
 export * from "./Card";
 export * from "./BorderlessCard";
+export * from "./GlitchingTitle";
 export * from "./H1";
 export * from "./H2";
 export * from "./H4";

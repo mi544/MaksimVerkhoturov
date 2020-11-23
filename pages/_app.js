b472be6833e5f7ea5367d70b0635415f6d0ef438
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-23 02:42:28 -0700

    adding meta tag, moving away from hooks

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-import MediaQueriesContext from "utils/MediaQueriesContext";
-import useWindowDimensions from "utils/hooks/useWindowDimensions";
+import Head from "next/head";
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -30,15 +29,19 @@ const theme = {
 };
 
 export default function App({ Component, pageProps }) {
-  const dimensions = useWindowDimensions();
   return (
     <>
-      <MediaQueriesContext.Provider value={dimensions}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </MediaQueriesContext.Provider>
+      <Head>
+        {/* Use minimum-scale=1 to enable GPU rasterization */}
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }

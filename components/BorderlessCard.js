c025db99dd713a0877b07b68d44502b458848f2b
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-23 03:14:35 -0700

    rewriting the card from scratch, media queries

--- components/BorderlessCard.js
+++ components/BorderlessCard.js
@@ -1,49 +1,45 @@
-import styled, { keyframes, css } from "styled-components";
-import { Card } from "components";
+import styled from "styled-components";
 
-const columnView = p => {
-  if (p.column) {
-    return css`
-      & > *:first-child {
-        width: 100%;
-        order: 2;
-        font-size: 1.5em;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-      & > *:last-child {
-        width: 100%;
-        clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 10%, 94% 0);
-      }
-    `;
-  } else {
-    return css`
-      & > *:first-child {
-        width: 50%;
-        font-size: 1.5em;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-      & > *:last-child {
-        width: 50%;
-        clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 10%, 94% 0);
-      }
-    `;
-  }
-};
-
-const BorderlessCard = styled(Card)`
-  flex-direction: ${p => (p.column ? "column" : "row")};
-  border: none;
-  box-shadow: none;
+const BorderlessCard = styled.section`
+  display: flex;
+  align-items: center;
+  margin: ${p => (p.margin ? p.margin : "0")};
+  height: ${p => (p.height ? p.height : "auto")};
+  width: ${p => (p.width ? p.width : "auto")};
+  flex-direction: column;
+  background-color: #ffffff00;
+  padding: 1% 2%;
   justify-content: space-evenly;
-  clip-path: none;
 
-  ${p => columnView(p)}
+  & > *:first-child {
+    width: 100%;
+    font-size: 1.5em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  & > *:last-child {
+    width: 100%;
+    clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 10%, 94% 0);
+  }
+
+  @media (min-width: 870px) {
+    flex-direction: row;
+
+    & > *:first-child {
+      width: 50%;
+      font-size: 1.5em;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    & > *:last-child {
+      width: 50%;
+      clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 10%, 94% 0);
+    }
+  }
 `;
 
 export { BorderlessCard };

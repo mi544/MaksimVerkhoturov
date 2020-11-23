e146c061cd4c5e0a00056fdbad0da56d3ef8dbdf
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-23 02:10:30 -0700

    moving away from hooks to media queries

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,7 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import SMLCheck from "utils/hooks/SmallMediumLargeCheck";
 import styled, { keyframes, css } from "styled-components";
-import MediaQueriesContext from "utils/MediaQueriesContext";
 
 const HeaderWrapper = styled.header`
   height: 100px;
@@ -16,46 +14,49 @@ const HeaderWrapper = styled.header`
   border-bottom: 2px solid ${p => p.theme.primaryColor};
 `;
 
-const NavBar = styled.nav`
-  display: flex;
-  justify-content: space-around;
-`;
-
 const NavBarWrapper = styled.ul`
+  text-align: right;
+  padding-right: 25px;
   list-style: none;
-  display: flex;
-  flex-direction: row;
+
+  @media (min-width: 915px) {
+    display: flex;
+    flex-direction: row;
+  }
 `;
 
 const NavBarItem = styled.li`
-  margin: auto 5px;
-  background-color: ${p => p.theme.secondaryColor};
-  box-shadow: ${p => (p.active ? `inset -4px -6px ${p.theme.backgroundColor}` : "none")};
-  border-radius: 6px;
-  padding: 10px 15px;
-  border: 1px solid white;
-  clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 23%, 85% 0);
-  transition: 0.2s;
-  user-select: none;
-  &:hover {
-    transform: scale(1.05, 1.05);
+  margin: 8px 0;
+
+  @media (min-width: 915px) {
+    margin: auto 5px;
+    background-color: ${p => p.theme.secondaryColor};
+    box-shadow: ${p =>
+      p.active ? `inset -4px -6px ${p.theme.backgroundColor}` : "none"};
+    border-radius: 6px;
+    padding: 10px 15px;
+    border: 1px solid white;
+    clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 23%, 85% 0);
+    transition: 0.2s;
+    user-select: none;
+    &:hover {
+      transform: scale(1.05, 1.05);
+    }
   }
 `;
 
 const NavLink = styled.a`
-  color: black;
-  /* text-shadow: 2px 1.5px 0px black, 2px 1px 0px black; */
-  font-size: 1.5em;
-  font-weight: 500;
-  &:link {
-    text-decoration: none;
+  font-size: 1.8em;
+
+  @media (min-width: 915px) {
+    color: black;
+    /* text-shadow: 2px 1.5px 0px black, 2px 1px 0px black; */
+    font-size: 1.5em;
+    font-weight: 500;
+    &:link {
+      text-decoration: none;
+    }
   }
-  /* &:visited {
-  }
-  &:hover {
-  }
-  &:active {
-  } */
 `;
 
 const HeaderTitle = styled.span`
@@ -96,6 +97,10 @@ const MenuButton = styled.div`
     height: 5px;
     margin: 6px 6px 0 4px;
   }
+
+  @media (min-width: 915px) {
+    display: none;
+  }
 `;
 
 const mobileMenuAppear = keyframes`
@@ -120,11 +125,7 @@ to {
   opacity: 0;
 }`;
 
-const MobileMenuLink = styled(NavLink)`
-  font-size: 1.8em;
-`;
-
-const MobileMenu = styled.div`
+const NavBar = styled.div`
   opacity: 0;
   position: fixed;
   top: 0;
@@ -160,90 +161,64 @@ const MobileMenu = styled.div`
 
   pointer-events: ${p => (p.show ? "auto" : "none")};
 
-  & > ul {
-    /* font-size: 1.6em; */
-    /* font-weight: 400; */
-    /* color: black; */
-    text-align: right;
-    padding-right: 25px;
-    list-style: none;
-  }
+  @media (min-width: 915px) {
+    display: flex;
+    justify-content: space-around;
+    position: relative;
 
-  & > ul > li {
-    margin: 8px 0;
+    opacity: 1;
+    flex-direction: column;
+    align-items: flex-end;
+    /* border: 1px ${p => p.theme.backgroundColor}; */
+    width: auto;
+    height: auto;
+    background: none;
+    clip-path: none;
+    animation: none;
   }
 `;
 
 const Header = () => {
   const router = useRouter();
-
-  const { height, width } = useContext(MediaQueriesContext);
   const [menuShown, setMenuShown] = useState(false);
   const [firstLoaded, setFirstLoaded] = useState(true);
   return (
     <HeaderWrapper>
       <HeaderTitle>Maksim Verkhoturov</HeaderTitle>
-      {SMLCheck(
-        <>
-          <MobileMenu show={menuShown} first={firstLoaded}>
-            <ul>
-              <li>
-                <Link href="/" passHref>
-                  <MobileMenuLink>Home</MobileMenuLink>
-                </Link>
-              </li>
-              <li>
-                <Link href="/about">
-                  <MobileMenuLink>About</MobileMenuLink>
-                </Link>
-              </li>
-              <li>
-                <Link href="/projects" passHref>
-                  <MobileMenuLink>Projects</MobileMenuLink>
-                </Link>
-              </li>
-              <li>
-                <Link href="/resources" passHref>
-                  <MobileMenuLink>Resources</MobileMenuLink>
-                </Link>
-              </li>
-            </ul>
-          </MobileMenu>
-          <MenuButton
-            onClick={() => {
-              setMenuShown(s => !s);
-              setFirstLoaded(false);
-            }}
-          >
-            <div></div>
-            <div></div>
-          </MenuButton>
-        </>,
-        <NavBar>
-          <NavBarWrapper>
-            <NavBarItem active={router.pathname === "/"}>
-              <Link href="/" passHref>
-                <NavLink>Home</NavLink>
-              </Link>
-            </NavBarItem>
-            <NavBarItem passHref active={router.pathname === "/about"}>
-              <Link href="/about">
-                <NavLink>About</NavLink>
-              </Link>
-            </NavBarItem>
-            <NavBarItem active={router.pathname === "/projects"}>
-              <Link href="/projects" passHref>
-                <NavLink>Projects</NavLink>
-              </Link>
-            </NavBarItem>
-            <NavBarItem active={router.pathname === "/resources"}>
-              <Link href="/resources" passHref>
-                <NavLink>Resources</NavLink>
-              </Link>
-            </NavBarItem>
-          </NavBarWrapper>
-        </NavBar>
-      )}
+
+      <NavBar show={menuShown} first={firstLoaded}>
+        <NavBarWrapper>
+          <NavBarItem>
+            <Link href="/" passHref>
+              <NavLink>Home</NavLink>
+            </Link>
+          </NavBarItem>
+          <NavBarItem>
+            <Link href="/about">
+              <NavLink>About</NavLink>
+            </Link>
+          </NavBarItem>
+          <NavBarItem>
+            <Link href="/projects" passHref>
+              <NavLink>Projects</NavLink>
+            </Link>
+          </NavBarItem>
+          <NavBarItem>
+            <Link href="/resources" passHref>
+              <NavLink>Resources</NavLink>
+            </Link>
+          </NavBarItem>
+        </NavBarWrapper>
+      </NavBar>
+      <MenuButton
+        onClick={() => {
+          setMenuShown(s => !s);
+          setFirstLoaded(false);
+        }}
+      >
+        <div></div>
+        <div></div>
+      </MenuButton>
     </HeaderWrapper>
   );
 };

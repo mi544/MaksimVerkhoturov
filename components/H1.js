d6b097a566109db14e17ea5c7c3e0ad5d2ddb85c
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-25 01:09:02 -0700

    font-size responsiveness

--- components/H1.js
+++ components/H1.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 const H1 = styled.h1`
-  font-size: 2.8em;
+  font-size: 1.5em;
+
+  @media (min-width: 870px) {
+    font-size: 2em;
+  }
+  @media (min-width: 1215px) {
+    font-size: 2.8em;
+  }
 `;
 
 export { H1 };
--- components/H4.js
+++ components/H4.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 const H4 = styled.h4`
-  font-size: 1em;
+  font-size: 0.9em;
+
+  @media (min-width: 870px) {
+    font-size: 1.1em;
+  }
+  @media (min-width: 1215px) {
+    font-size: 1.5em;
+  }
 `;
 
 export { H4 };

7a50239b2ff67f83ae342ce7b7351da1ac27659d
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-27 06:21:28 -0700

    additional styles for better responsiveness

--- components/H2.js
+++ components/H2.js
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
 const H2 = styled.h2`
-  font-size: 2em;
+  font-size: 1.2em;
+  line-height: ${p => (p.line ? p.line : "auto")};
+
+  @media (min-width: 870px) {
+    font-size: 1.5em;
+  }
+  @media (min-width: 1215px) {
+    font-size: 2em;
+  }
 `;
 
 export { H2 };

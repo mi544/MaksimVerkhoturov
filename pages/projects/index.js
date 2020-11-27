e835850049222fe3b15b2a1542659a28bd033bd2
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-27 03:01:22 -0700

    adding working title

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -104,6 +104,46 @@ const Projects = () => {
               </div>
             </BorderlessCard>
           </Row>
+          <Row center>
+            <BorderlessCard
+              appearDuration={1}
+              delayDuration={0.1}
+              textDelayDuration={0.2}
+              width="90%"
+              margin="30px auto auto auto"
+            >
+              <div>
+                <GlitchingTitle margin="2px auto 20px auto">
+                  <H1>Working Title</H1>
+                  <H1>Working Title</H1>
+                  <H1>Working Title</H1>
+                  <H1>Working Title</H1>
+                </GlitchingTitle>
+
+                <article>
+                  <p>
+                    最近、よく耳にする『糖質制限』なる言葉。大豆粉を購入したので、おからパウダーと合わせ、買い置きのココナッツも加えてホットケーキ（パンケーキ）を焼いてみることにしました。
+                  </p>
+                  <p>
+                    牛乳→水にする時はバターと黒蜜だけでも美味しいですがトッピングは栗や生クリームなども♫お好きな和風を楽しんで下さい^o^
+                  </p>
+                  <p>
+                    の時さっくり混ぜるとふわっとした仕上がりになります。そば粉を入れずに薄力粉のみでもOK
+                  </p>
+                </article>
+              </div>
+              <div>
+                <Image
+                  src="/images/tortoise-mobile.png"
+                  alt="tortoise"
+                  width={600}
+                  height={600}
+                  layout="intrinsic"
+                  quality={100}
+                />
+              </div>
+            </BorderlessCard>
+          </Row>
         </Main>
         <Footer />
       </Container>

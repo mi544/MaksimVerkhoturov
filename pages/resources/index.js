5105a3bd414e555b01d3d6b8636788be71c93ec5
Maksim Verkhoturov <sd32@protonmail.com>
2020-11-27 03:01:43 -0700

    adding basic info to resources

--- pages/resources/index.js
+++ pages/resources/index.js
@@ -1,6 +1,16 @@
 import Head from "next/head";
+import Image from "next/image";
 
-import { Header, Main, Container, Footer } from "components";
+import {
+  Header,
+  Main,
+  Container,
+  Footer,
+  Row,
+  BorderlessCard,
+  GlitchingTitle,
+  H1
+} from "components";
 
 const Resources = () => {
   return (
@@ -11,7 +21,46 @@ const Resources = () => {
       <Container>
         <Header />
         <Main>
-          <h1>Resources</h1>
+          <Row center>
+            <BorderlessCard
+              appearDuration={1}
+              delayDuration={0.1}
+              textDelayDuration={0.2}
+              width="90%"
+              margin="30px auto auto auto"
+            >
+              <div>
+                <GlitchingTitle margin="2px auto 20px auto">
+                  <H1>Mood Music</H1>
+                  <H1>Mood Music</H1>
+                  <H1>Mood Music</H1>
+                  <H1>Mood Music</H1>
+                </GlitchingTitle>
+
+                <article>
+                  <p>
+                    最近、よく耳にする『糖質制限』なる言葉。大豆粉を購入したので、おからパウダーと合わせ、買い置きのココナッツも加えてホットケーキ（パンケーキ）を焼いてみることにしました。
+                  </p>
+                  <p>
+                    牛乳→水にする時はバターと黒蜜だけでも美味しいですがトッピングは栗や生クリームなども♫お好きな和風を楽しんで下さい^o^
+                  </p>
+                  <p>
+                    の時さっくり混ぜるとふわっとした仕上がりになります。そば粉を入れずに薄力粉のみでもOK
+                  </p>
+                </article>
+              </div>
+              <div>
+                <Image
+                  src="/images/tortoise-mobile.png"
+                  alt="tortoise"
+                  width={600}
+                  height={600}
+                  layout="intrinsic"
+                  quality={100}
+                />
+              </div>
+            </BorderlessCard>
+          </Row>
         </Main>
         <Footer />
       </Container>

d2e59a2a288200020618e4a8afbe9959f065d81f
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-12 05:48:26 -0700

    adding HighlightText component

---
+++ components/HighlightText.js
@@ -0,0 +1,31 @@
+import styled, { keyframes, css } from "styled-components";
+
+const colorChange = keyframes`
+0% {
+  text-shadow: -4px 2px #272727;
+}
+
+50% {
+  text-shadow: -4px 2px #2e3e7b;
+}
+
+100% {
+  text-shadow: -4px 2px #272727;
+}
+`;
+
+// accepts only 4 h tags for now
+const HighlightText = styled.span`
+  margin: ${p => (p.margin ? p.margin : "auto")};
+
+  text-shadow: -4px 2px #272727;
+  ${p =>
+    p.noAnimate
+      ? ""
+      : css`
+          animation: ${colorChange} 10s ${p.delay ? p.delay : ""} infinite
+            cubic-bezier(0, 0.38, 1, 0.08);
+        `}
+`;
+
+export { HighlightText };

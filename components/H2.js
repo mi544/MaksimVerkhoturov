f0ab3c17f5aad293c5b3699c807ca8b283d3b20f
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-16 10:53:44 -0700

    adds margin, changes font size

--- components/H2.js
+++ components/H2.js
@@ -1,11 +1,17 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const H2 = styled.h2`
-  font-size: 1.5em;
+  font-size: 1.7em;
   line-height: ${p => (p.line ? p.line : "normal")};
+  ${p =>
+    p.margin
+      ? css`
+          margin: ${p.margin};
+        `
+      : ""}
 
   @media (min-width: 870px) {
-    font-size: 1.5em;
+    font-size: 2em;
   }
   @media (min-width: 1215px) {
     font-size: 2.5em;

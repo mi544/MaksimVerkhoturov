843c674beee8ab4b81cde3849e1e2b324931df00
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-16 11:39:04 -0700

    changes resources to TBD

--- pages/resources/index.js
+++ pages/resources/index.js
@@ -22,40 +22,7 @@ const Resources = () => {
         <Header />
         <Main>
           <Row center>
-            <BorderlessCard
-              appearDuration={1}
-              delayDuration={0.1}
-              textDelayDuration={0.2}
-              width="90%"
-              margin="30px auto auto auto"
-            >
-              <div>
-                <H1>Books</H1>
-
-                <article>
-                  <p>
-                    最近、よく耳にする『糖質制限』なる言葉。大豆粉を購入したので、おからパウダーと合わせ、買い置きのココナッツも加えてホットケーキ（パンケーキ）を焼いてみることにしました。
-                  </p>
-                  <p>
-                    牛乳→水にする時はバターと黒蜜だけでも美味しいですがトッピングは栗や生クリームなども♫お好きな和風を楽しんで下さい^o^
-                  </p>
-                  <p>
-                    の時さっくり混ぜるとふわっとした仕上がりになります。そば粉を入れずに薄力粉のみでもOK
-                  </p>
-                </article>
-              </div>
-              <div>
-                <Image
-                  src="/images/tortoise-mobile.png"
-                  alt="tortoise"
-                  width={600}
-                  height={600}
-                  layout="intrinsic"
-                  quality={100}
-                  priority
-                />
-              </div>
-            </BorderlessCard>
+            <H1>TBD</H1>
           </Row>
         </Main>
         <Footer />

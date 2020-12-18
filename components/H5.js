310bbd6fc8c9861c541b5f9136ac578f9efeb37c
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-18 02:55:52 -0700

    adds H5

---
+++ components/H5.js
@@ -0,0 +1,27 @@
+import styled, { css } from "styled-components";
+
+const H5 = styled.h5`
+  font-size: 1em;
+  line-height: ${p => (p.line ? p.line : "normal")};
+  ${p =>
+    p.margin
+      ? css`
+          margin: ${p.margin};
+        `
+      : ""}
+  ${p =>
+    p.center
+      ? css`
+          text-align: center;
+        `
+      : ""}
+
+  @media (min-width: 870px) {
+    font-size: 1.2em;
+  }
+  @media (min-width: 1215px) {
+    font-size: 1.5em;
+  }
+`;
+
+export { H5 };

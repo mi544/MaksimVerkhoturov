7390e4a87922294b704db8d49b5800da12eaa6d8
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-18 02:56:54 -0700

    adjusting exports

--- components/index.js
+++ components/index.js
@@ -9,6 +9,7 @@ export * from "./H1";
 export * from "./H2";
 export * from "./H3";
 export * from "./H4";
+export * from "./H5";
 export * from "./Align";
 export * from "./Button";
 export * from "./StyledLink";
